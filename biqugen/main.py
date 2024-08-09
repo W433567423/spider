e1061495cb46baf3b944a6e5918bc627bcb73fe5
tutu <t433567423@163.com>
2024-08-09 15:47:51 +0800

    Refactor main.py to use get_not_crawled_novel_id_list and add flag to control crawling loop

--- biqugen/main.py
+++ biqugen/main.py
@@ -1,11 +1,18 @@
 from scrapy.cmdline import execute  # type: ignore
 import time
 from biqugen.utils import console
+from biqugen.db import get_not_crawled_novel_id_list
 
 # execute(["scrapy", "crawl", "get-list"])
 
 if __name__ == "__main__":
     # 每执行完一次，5s后执行下一次
-    while True:
-        execute(["scrapy", "crawl", "get-chapter"])
-        time.sleep(5)
+    flag = True
+    while flag:
+        next = get_not_crawled_novel_id_list(1)
+        if not next:
+            console.log("所有小说爬取完毕")
+            flag = False
+        else:
+            execute(["scrapy", "crawl", "get-chapter"])
+            time.sleep(5)
